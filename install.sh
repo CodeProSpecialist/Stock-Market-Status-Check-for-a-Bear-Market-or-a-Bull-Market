a561c00cfa920b0cafd94d2012b08dc6b04869b6
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-11-29 05:49:48 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Update package list
+sudo apt-get update
+
+# Install Python3
+sudo apt-get install -y python3
+
+# Install pip3
+sudo apt-get install -y python3-pip
+
 # Install yfinance
 pip3 install yfinance
 
